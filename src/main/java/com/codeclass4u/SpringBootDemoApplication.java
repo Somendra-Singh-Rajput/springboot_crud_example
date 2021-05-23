70d7ae1c092cb597176956372bafa3e491a33bef
Somendra Singh Rajput <singhsomendra420@gmail.com>
2021-05-23 22:40:20 +0530

    Update SpringBootDemoApplication.java

--- src/main/java/com/codeclass4u/SpringBootDemoApplication.java
+++ src/main/java/com/codeclass4u/SpringBootDemoApplication.java
@@ -10,6 +10,7 @@ public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("Somendra Singh Rajput");
 	}
 
 }
